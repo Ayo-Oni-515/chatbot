b96edc69ba021df65502926bfc8fb577db2c6f16
ayodeji oni <ayodejioni1505@gmail.com>
2025-08-12 13:02:31 +0100

    update ijmplementation

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI, HTTPException  # noqa
 from pydantic import BaseModel
+from typing import Literal
+from uuid import UUID
+from datetime import datetime
 
 from chatbot import ChatBot
 
 
 class PromptModel(BaseModel):
     user_id: str
+    role: Literal["student", "teacher"]
+    session_id: UUID
     prompt: str
+
+
+class PromptResponseModel(BaseModel):
+    user_id: str
+    role: Literal["student", "teacher"]
+    session_id: UUID
+    prompt: str
+    timestamp: datetime
 
 
 app = FastAPI()
@@ -17,16 +30,18 @@ bot = ChatBot().acompile()
 
 @app.post('/chatbot')
 async def chatbot(prompt_data: PromptModel):
-    config = {"configurable": {"thread_id": prompt_data.user_id}}
+    config = {"configurable": {"thread_id": prompt_data.session_id}}
     response = await bot.ainvoke({"messages": prompt_data.prompt}, config)
 
     return {
         "user_id": prompt_data.user_id,
-        "response": (response["messages"][-1]).content}
+        "role": prompt_data.role,
+        "response": (response["messages"][-1]).content
+        }
 
 
 @app.get('/health-check')
 async def healthcheck():
     return {
-        "output": "Educify chatbot API is active!"
+        "output": "Chatbot API is active!"
     }
